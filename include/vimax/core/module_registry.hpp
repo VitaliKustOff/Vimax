a691ac1f21087710854ff2076655ec6e8a6f672f
VitaliKustOff <Vimax77@gmail.com>
2026-07-20 00:20:18 +0300

    Add subsystem initialization lifecycle

--- include/vimax/core/module_registry.hpp
+++ include/vimax/core/module_registry.hpp
@@ -1,5 +1,11 @@
 #pragma once
-#include "vimax/core/interfaces.hpp"
-#include <memory>
-#include <unordered_map>
-namespace vimax {class ModuleRegistry{public:void register_module(std::shared_ptr<IModule>);std::shared_ptr<IModule> find_for_intent(const std::string&) const;private:std::unordered_map<std::string,std::shared_ptr<IModule>> by_intent_;};}
+
+namespace vimax {
+
+class ModuleRegistry
+{
+public:
+    void initialize();
+};
+
+}
--- src/module_registry.cpp
+++ src/module_registry.cpp
@@ -1,3 +1,9 @@
 #include "vimax/core/module_registry.hpp"
-#include <stdexcept>
-namespace vimax {void ModuleRegistry::register_module(std::shared_ptr<IModule> m){if(!m)throw std::invalid_argument("module must not be null");for(const auto&i:m->supported_intents())by_intent_[i]=m;}std::shared_ptr<IModule> ModuleRegistry::find_for_intent(const std::string&i) const{auto it=by_intent_.find(i);return it==by_intent_.end()?nullptr:it->second;}}
+
+namespace vimax {
+
+void ModuleRegistry::initialize()
+{
+}
+
+}
